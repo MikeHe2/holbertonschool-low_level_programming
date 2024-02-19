c25eadb4adbfc216a511374f67c2ee0c19a37304
Michael A. Hernandez <michael.he30@hotmail.com>
2024-02-19 18:17:51 -0500

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-put(Programming is like building a multilingual puzzle)
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
