4c81bf638ca5935add683b19b27e43c210008ae8
Michael A. Hernandez <michael.he30@hotmail.com>
2024-02-20 19:23:09 -0500

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ *
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
