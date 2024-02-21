d1190de75ca7cc4b03f20b704c2c4cb20d55dd76
Michael A. Hernandez <michael.he30@hotmail.com>
2024-02-20 20:37:48 -0500

    100-print_comb3.c

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -8,24 +8,24 @@
  */
 int main(void)
 {
-	int digit1, digit2;
+  int tens;
+  int ones;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+  for (tens = 0; tens <= 9; tens++)
+    {
+      for (ones = tens + 1; ones <= 9; ones++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+	  putchar(tens + '0');
+	  putchar(ones + '0');
 
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-			
-			putchar(',');
-			putchar(' ');
-		}
+	  if (tens < 8)
+	    {
+	      putchar(',');
+	      putchar(' ');
+	    }
 	}
+    }
+  putchar('\n');
 
-	putchar('\n');
-
-	return (0);
+  return (0);
 }
