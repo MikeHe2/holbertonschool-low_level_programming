b2719ac52e0ff4d9bdc5dad0a0f01c0c7a614153
Michael A. Hernandez <michael.he30@hotmail.com>
2024-02-21 14:51:14 -0500

    0-putchar.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Succes)
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+		for (letter = 'a'; letter == 'z'; letter++)
+		{
+			putchar(letter);
+		}
+
+		putchar('\n');
+
+	return (0);
+}
+
