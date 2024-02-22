3812672f54c73dd06aea885ab9ff28b62966948b
Michael A. Hernandez <michael.he30@hotmail.com>
2024-02-22 13:21:07 -0500

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _isdigit - Shows 1 if the input is a
+* number. Another cases, shows 0
+*
+* @c: The char
+*
+* Return: 1 for lowercase. 0 for the rest.
+*/
+
+int _isupper(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
