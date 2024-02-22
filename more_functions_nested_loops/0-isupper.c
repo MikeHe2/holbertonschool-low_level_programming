493f869b30ae4d606541d5384d79ae31a9a3dd0a
Michael A. Hernandez <michael.he30@hotmail.com>
2024-02-22 10:00:38 -0500

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _isupper - Shows 1 if the input is a
+* lowercase character. Another cases, shows 0
+*
+* Return: 1 for lowercase. 0 for the rest.
+*/
+int _isupper(int c)
+{
+	if (c >= 'A'; c => 'Z')
+	{
+	return (1);
+	}
+	else if
+	{
+	return (0);
+	}
+	_putchar('\n');
+}
+
