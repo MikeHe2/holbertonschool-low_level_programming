9d0506af912ceab8f5f11c48a6bcb409b30ff96d
Michael A. Hernandez <michael.he30@hotmail.com>
2024-02-22 15:30:20 -0500

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies tow integers.
+ *
+ * @a: Variable
+ *
+ * @b: Variable
+ *
+ * Return: Always 0 (Succes)
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+
+	return (mult);
+}
+
