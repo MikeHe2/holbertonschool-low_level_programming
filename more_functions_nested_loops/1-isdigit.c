0a4b326db3eb02ca9419190dc7f1b42531e72467
Michael A. Hernandez <michael.he30@hotmail.com>
2024-02-22 14:05:35 -0500

    1-isdigit.c

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -4,14 +4,14 @@
 * _isdigit - Shows 1 if the input is a
 * number. Another cases, shows 0
 *
-* @c: The char
+* @c: hola mun
 *
 * Return: 1 for lowercase. 0 for the rest.
 */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
