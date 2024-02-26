1886bc2d829686acf7eead67067ab1b3286fbfb2
Michael A. Hernandez <michael.he30@hotmail.com>
2024-02-26 12:28:47 -0500

    1-swap.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - Function that swaps the values of two integers.
+ *
+ * @a: Pointer's name
+ * @b: Pointer's name
+ */
+
+void swap_int(int *a, int *b)
+{
+
+	*a = 42;
+	*b = 98;
+}
