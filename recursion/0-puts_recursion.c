f6280a791993a714ceb186fe7246088e3bc7b628
Michael A. Hernandez <michael.he30@hotmail.com>
2024-03-04 18:49:21 -0500

    0-puts_recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Fuction that prints a string, followed by a new line.
- *
- * @s: string name.
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: string name
+ * 
+ * Return: no return.
  */
-
-void _puts_recursion(char *s);
-
+void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s++);
+		_puts_recursion(s + 1);
 	}
 	else
-	{
-		_putchar("\n");
-	}
+		_putchar('\n');
 }
