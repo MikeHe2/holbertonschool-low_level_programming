1f64db2f86326ed84c0e8fb26fa353c6d9cbf786
Michael A. Hernandez <michael.he30@hotmail.com>
2024-03-11 11:21:35 -0400

    Escentials

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,8 +8,4 @@ float age;
 char *owner;
 }
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-
 #endif
