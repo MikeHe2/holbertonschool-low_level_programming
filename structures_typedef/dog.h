d8d1a1d730b72598d6e3198ef9fd2cb31258faf0
Michael A. Hernandez <michael.he30@hotmail.com>
2024-03-11 20:16:08 -0400

    dog.h fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,5 +15,13 @@ char *name;
 float age;
 char *owner;
 };
+typedef struct dog dog_t;
+
+/*This are the prototypes of the proect.*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
