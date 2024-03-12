08a585a72decf669f34a543d9438d7da004a0b13
Michael A. Hernandez <michael.he30@hotmail.com>
2024-03-11 23:33:40 -0400

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 
 	if (d->name == NULL)
 	{
-		printf("Name: (nil)");
+		printf("Name: (nil)\n");
 	}
 	else
 	{
@@ -28,7 +28,7 @@ void print_dog(struct dog *d)
 
 	if (d->age == '\0')
 	{
-		printf("Age: (nil)");
+		printf("Age: (nil)\n");
 	}
 	else
 	{
@@ -37,7 +37,7 @@ void print_dog(struct dog *d)
 
 	if (d->owner == NULL)
 	{
-		printf("Owner: (nil)");
+		printf("Owner: (nil)\n");
 	}
 	else
 	{
