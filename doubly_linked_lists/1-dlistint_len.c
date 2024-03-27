be2ff1cbbaf4f2915af1ff5606606719266e304b
Michael A. Hernandez <michael.he30@hotmail.com>
2024-03-27 18:30:55 -0400

    Fix betty comments

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,12 @@
 #include "lists.h"
 
 /**
- * dlistint_len - 
+ * dlistint_len - Functin that returns the number
+ * of elements in a linked dlistint_t list.
+ *
+ * @h: Head of the list.
+ *
+ * Return: Number of elements.
  */
 
 size_t dlistint_len(const dlistint_t *h)
