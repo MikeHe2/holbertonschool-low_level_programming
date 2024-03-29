134648a6cd5becbe0420bf5988d22cf2230fe760
Michael A. Hernandez <michael.he30@hotmail.com>
2024-03-28 23:16:02 -0400

    fix betty comments

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,6 +1,11 @@
 #include "main.h"
 /**
+ * create_file - Function that creates a file.
  *
+ * @filename: Is the name of the file to be created.
+ * @text_content: Is a NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
  */
 
 int create_file(const char *filename, char *text_content)
